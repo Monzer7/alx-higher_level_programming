90dee51ffa40625a40bda8f6a58cb3dfaee1c5bc
Monzer7 <monzer-7@hotmail.com>
2023-11-29 15:22:36 -0800

    dszx

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,8 +3,11 @@ def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0:
             print("Fizz", end=' ')
+            continue
         elif i % 5 == 0:
             print("Buzz", end=' ')
+            continue
         elif i % 5 == 0 and i % 3 == 0:
             print("FizzBuzz", end=' ')
+            continue
         print(i, end=' ')
