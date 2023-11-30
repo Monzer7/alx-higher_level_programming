256df64f493e2efdc475af506e647e97b6495a45
Monzer7 <monzer-7@hotmail.com>
2023-11-30 14:34:12 -0800

    task3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,9 @@ if __name__ == "__main__":
     num = len(sys.argv) - 1
     if num == 0:
         print("0 arguments.")
+    elif num == 1:
+        print("{} argument".format(num))
+        print("{}: {}".format(num, sys.argv[1]))
     else:
         print("{} arguments:".format(num))
         for i in range(num):
