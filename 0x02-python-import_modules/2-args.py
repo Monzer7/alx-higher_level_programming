b473a14ec6e009fc0846e1997f7543e261f28de6
Monzer7 <monzer-7@hotmail.com>
2023-11-30 14:27:51 -0800

    task3

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv) - 1
+    if num == 0:
+        print("0 arguments.")
+    else:
+        print("{}: arguments:".format(num))
+        for i in range(num):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))
