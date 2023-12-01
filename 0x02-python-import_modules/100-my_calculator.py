a44eacb171094744ce67b3352f3c8236b5bf481e
Monzer7 <monzer-7@hotmail.com>
2023-11-30 16:30:33 -0800

    dsds

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,14 +3,14 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     import sys
-    num1 = int(sys.argv[1])
-    num2 = int(sys.argv[3])
     num = len(sys.argv)
     if num != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
     else:
+        num1 = int(sys.argv[1])
+        num2 = int(sys.argv[3])
         if sys.argv[2] == '+':
             print("{} + {} = {}".format(num1, num2, add(num1, num2)))
 
