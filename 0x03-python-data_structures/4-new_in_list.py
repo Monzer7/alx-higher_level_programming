3f09a3e1843b5670daa93936f442a4461e02c1eb
Monzer7 <monzer-7@hotmail.com>
2023-12-04 03:42:05 -0800

    copy

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is not None:
-        cp = my_list.copy()
-        for i in range(len(my_list)):
-            if i == idx:
-                cp[i] = element
-                return cp
+
+    cp = my_list.copy()
+    if idx > 0 and idx < len(my_list):
+        if my_list is not None:
+            for i in range(len(my_list)):
+                if i == idx:
+                    cp[i] = element
+                    return cp
+    else:
+        return cp
