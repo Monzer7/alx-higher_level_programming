a63f953932a52ba78abbe48cb5931b2162d6caa8
Monzer7 <monzer-7@hotmail.com>
2023-12-04 13:33:12 -0800

    max

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/pythone3
+def max_integer(my_list=[]):
+    if not my_list:
+        return None
+    else:
+        high = my_list[0]
+        for i in range(len(my_list)):
+            if my_list[i] > high:
+                high = my_list[i]
+        return high
