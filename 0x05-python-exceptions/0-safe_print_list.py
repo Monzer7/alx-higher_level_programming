30ffa5fa108f7e78ba2d38364b72458e9fd93348
Monzer7 <monzer-7@hotmail.com>
2023-12-18 00:59:44 -0800

    first commit

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    m = 0
+    try:
+        while m < x :
+            print("{}".format(my_list[m]),end="")
+            m = m + 1
+        print()
+    except IndexError:
+        print()
+        return m
+    return m
